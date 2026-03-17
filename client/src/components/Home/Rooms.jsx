7db99743a9b517753223bbf71da7c66a9326a77a
khairulhub <iubat21103033@gmail.com>
2026-03-17 15:39:17 +0600

    create a custom hook useaxioscommon it provide only web url link. change file are rooms and rooms details, server site create single room api url link, and it call in the client site

--- client/src/components/Home/Rooms.jsx
+++ client/src/components/Home/Rooms.jsx
@@ -4,10 +4,10 @@ import Container from '../Shared/Container'
 import Heading from '../Shared/Heading'
 import LoadingSpinner from '../Shared/LoadingSpinner'
 import { useQuery } from '@tanstack/react-query'
-import useAxiosSecure from '../../hooks/useAxiosSecure'
+import useAxiosCommon from '../../hooks/useAxiosCommon'
 
 const Rooms = () => {
-  const axiosSecure = useAxiosSecure()
+  const axiosCommon = useAxiosCommon()
   // const [rooms, setRooms] = useState([])
   // const [loading, setLoading] = useState(false)
   //./rooms.json
@@ -25,7 +25,7 @@ const Rooms = () => {
   const {data:rooms = [], isLoading} = useQuery({
     queryKey: ['rooms'],
     queryFn: async () => {
-      const {data} = await axiosSecure.get('/rooms')
+      const {data} = await axiosCommon.get('/rooms')
       return data
     }
   })
