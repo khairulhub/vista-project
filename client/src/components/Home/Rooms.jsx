80db5d13a5d9d5cea1f1c54628338c89a557b368
khairulhub <iubat21103033@gmail.com>
2026-03-17 15:04:00 +0600

    tanstck query use koira data fatch koira data show kora hoicey rooms.jsx a client site a

--- client/src/components/Home/Rooms.jsx
+++ client/src/components/Home/Rooms.jsx
@@ -3,23 +3,37 @@ import Card from './Card'
 import Container from '../Shared/Container'
 import Heading from '../Shared/Heading'
 import LoadingSpinner from '../Shared/LoadingSpinner'
+import { useQuery } from '@tanstack/react-query'
+import useAxiosSecure from '../../hooks/useAxiosSecure'
 
 const Rooms = () => {
-  const [rooms, setRooms] = useState([])
-  const [loading, setLoading] = useState(false)
+  const axiosSecure = useAxiosSecure()
+  // const [rooms, setRooms] = useState([])
+  // const [loading, setLoading] = useState(false)
   //./rooms.json
 
-  useEffect(() => {
-    setLoading(true)
-    fetch(`http://localhost:8000/rooms`)
-      .then(res => res.json())
-      .then(data => {
-        setRooms(data)
-        setLoading(false)
-      })
-  }, [])
+   // useEffect(() => {
+  //   setLoading(true)
+  //   fetch(`http://localhost:8000/rooms`)
+  //     .then(res => res.json())
+  //     .then(data => {
+  //       setRooms(data)
+  //       setLoading(false)
+  //     })
+  // }, [])
 
-  if (loading) return <LoadingSpinner />
+  const {data:rooms = [], isLoading} = useQuery({
+    queryKey: ['rooms'],
+    queryFn: async () => {
+      const {data} = await axiosSecure.get('/rooms')
+      return data
+    }
+  })
+
+ 
+
+  // if (loading) return <LoadingSpinner />
+  if (isLoading) return <LoadingSpinner />
 
   return (
     <Container>
