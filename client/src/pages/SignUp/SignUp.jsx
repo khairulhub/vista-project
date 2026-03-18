23ba4c69a971bdaaab64078a745c1727d135371b
khairulhub <iubat21103033@gmail.com>
2026-03-18 12:48:23 +0600

    sign in done by using email pass

--- client/src/pages/SignUp/SignUp.jsx
+++ client/src/pages/SignUp/SignUp.jsx
@@ -1,7 +1,47 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { FcGoogle } from 'react-icons/fc'
+import useAuth from '../../hooks/useAuth'
+import { toast } from 'react-hot-toast'
+import { TbFidgetSpinner } from "react-icons/tb";
+import axios from 'axios'
 
 const SignUp = () => {
+  const navigate = useNavigate()
+  const {createUser, signInWithGoogle, updateUserProfile, loading, setLoading} = useAuth() 
+  const handlesubmit = async e =>{
+    e.preventDefault()
+    const form = e.target
+    const name = form.name.value
+    const email = form.email.value
+    const password = form.password.value
+    const image = form.image.files[0]
+    const formData = new FormData()
+    formData.append('image', image)
+    
+
+    try{
+      setLoading(true)
+      //upload image to imgbb and get the image url
+      const {data } = await axios.post ( `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`, 
+        formData
+      )
+      // console.log(data)
+      //user registration with email and password
+      const result = await createUser(email, password)
+      //update user profile with name and image url
+      await updateUserProfile(name, data.data.display_url)
+      navigate('/')
+      toast.success('User created successfully')
+    
+      // console.log(result.user)
+    }
+    
+    catch(error){
+      toast.error(error.message)
+      setLoading(false)
+    }
+    
+  }
   return (
     <div className='flex justify-center items-center min-h-screen'>
       <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
@@ -9,10 +49,7 @@ const SignUp = () => {
           <h1 className='my-3 text-4xl font-bold'>Sign Up</h1>
           <p className='text-sm text-gray-400'>Welcome to StayVista</p>
         </div>
-        <form
-          noValidate=''
-          action=''
-          className='space-y-6 ng-untouched ng-pristine ng-valid'
+        <form onSubmit={handlesubmit} className='space-y-6 ng-untouched ng-pristine ng-valid'
         >
           <div className='space-y-4'>
             <div>
@@ -74,10 +111,11 @@ const SignUp = () => {
 
           <div>
             <button
+            disabled={loading}
               type='submit'
               className='bg-rose-500 w-full rounded-md py-3 text-white'
             >
-              Continue
+              {loading ? <TbFidgetSpinner  className='animate-spin mx-auto'/> : 'Continue'}
             </button>
           </div>
         </form>
