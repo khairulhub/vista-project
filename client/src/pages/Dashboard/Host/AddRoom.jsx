86df327bc129327390b1f08908e38a074e05b586
khairulhub <iubat21103033@gmail.com>
2026-03-21 13:05:01 +0600

    dashboard setup done with sidebar and outlet.

---
+++ client/src/pages/Dashboard/Host/AddRoom.jsx
@@ -0,0 +1,11 @@
+
+
+const AddRoom = () => {
+    return (
+        <div>
+            <h1>Welcome to Dashboard : Add Room</h1>
+        </div>
+    );
+}
+
+export default AddRoom;
---
+++ client/src/pages/Dashboard/Host/MyListings.jsx
@@ -0,0 +1,11 @@
+
+const MyListings = () => {
+    return (
+        <div>
+            <h1>Welcome to Dashboard : My Listings</h1>
+
+        </div>
+    );
+}
+
+export default MyListings;
